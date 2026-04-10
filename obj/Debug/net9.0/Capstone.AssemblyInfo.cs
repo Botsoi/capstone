b291a0eb2604c7679365258815c1e7caf6bece5a
DrakeDev23 <maccogoth@gmail.com>
2026-04-10 20:36:27 +0800

    updated icon logo pre

--- obj/Debug/net9.0/Capstone.AssemblyInfo.cs
+++ obj/Debug/net9.0/Capstone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab607af247ba7a90e125e020faf7aeb442b83bf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28864e7179cd56cc9a324eb207d44f03c77df107")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
